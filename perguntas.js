d572b0ac0c4edb7932d1d5c23dcad600962e94db
Jgkim2007 <00001123496080sp@al.educacao.sp.gov.br>
2024-12-05 12:10:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Biologia',
+    'Para que serve a juba do leão?',
+    'Serve para proteger o felino contra machucados durante as brigas com outros machos. '
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o maior animal do mundo?',
+    'A baleia-azul é o maior animal do planeta, podendo atingir cerca de 30 metros de comprimento.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o maior animal do mundo terrestre?',
+    'Elefante - Atinge, em média, 3,2 metros de altura e pesa 6 toneladas.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a maior floresta do mundo?',
+    'Conhecida como Pulmão do mundo, a Floresta Amazônica é a maior floresta do mundo, com aproximadamente 7 milhões de quilômetros quadrados.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior continente do mundo?',
+    'A Ásia é o maior continente em extensão territorial e em população absoluta entre todos do mundo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior oceano do mundo?',
+    'O Pacífico é o maior oceano do nosso planeta, responsável por recobrir quase que um terço da superfície terrestre, perfazendo uma área de aproximadamente 165 milhões de km2..'
+)
+
+criaCartao(
+    'Pokémon',
+    'Qual tipo de pokémon é mais forte contra tipo dragão?',
+    'São os tipo fada.'
+)
+
+criaCartao(
+    'Pokémon',
+    'Qual pokémon criou o mundo?',
+    'Arceus.'
+)
+
+criaCartao(
+    'Pokémon',
+    'Qual é o Pseudo-Lendário da terceira geração?',
+    'È o Salamence.'
+)
